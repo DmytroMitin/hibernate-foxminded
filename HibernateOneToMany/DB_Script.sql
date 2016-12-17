f9f2999301578eff235280691108d722bc58f79a
DmytroMitin <dmitin3@gmail.com>
2016-12-17 17:19:06 +0200

    many to one

--- HibernateOneToMany/DB_Script.sql
+++ HibernateOneToMany/DB_Script.sql
@@ -1,24 +1,24 @@
 --<ScriptOptions statementTerminator=";"/>
 
-drop table if exists training.department;
-CREATE TABLE training.department (
+drop table if exists public.department;
+CREATE TABLE public.department (
 	department_id BIGINT PRIMARY KEY ,
 	dept_name VARCHAR(50) NOT NULL DEFAULT '0'
 	
 );
 
 
-drop table if exists training.employee;
+drop table if exists public.employee;
 
-CREATE TABLE training.employee (
+CREATE TABLE public.employee (
 	employee_id BIGINT PRIMARY KEY,
 	firstname VARCHAR(50) NULL DEFAULT NULL,
 	lastname VARCHAR(50) NULL DEFAULT NULL,
 	birth_date DATE NULL DEFAULT NULL,
 	cell_phone VARCHAR(15) NULL DEFAULT NULL,
-	department_id BIGINT references training.department(department_id)
+	department_id BIGINT references public.department(department_id)
 );
 
-create sequence training.hibernate_sequences;
-create sequence training.hibernate_sequence;
+create sequence public.hibernate_sequences;
+create sequence public.hibernate_sequence;
 
